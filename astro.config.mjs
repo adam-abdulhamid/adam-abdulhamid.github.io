e12ed2fbc780825455ee9e169a7c6ca7f88a0173
Adam Abdulhamid <adam.m.abdulhamid@gmail.com>
2025-08-18 10:05:51 -0700

    Fix gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
